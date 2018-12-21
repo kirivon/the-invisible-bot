afbc074b257a690210c5a12ac47ecdae9f662654
alexisveryreal <44214923+alexisveryreal@users.noreply.github.com>
2018-12-20 23:39:57 -0800

    Added punches+Info functions (with error)

--- cogs/AlexTest.py
+++ cogs/AlexTest.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 
 
 class AlexTest:
-
+    """Class is just basic bot and user interaction"""
+    
     def __init__(self, bot):
         self.bot = bot
 
@@ -14,8 +15,26 @@ class AlexTest:
         author = ctx.author.name
         server = ctx.guild.name
         await ctx.send("Pong from {} from {}!".format(author, server))
+    
+    @commands.command()
+    async def punch(self, ctx, user: discord.Member):
+        """Punches the user mepntions"""
+        await ctx.send("ONE PUNCH! And " + user.mention + "is out! ლ(ಠ益ಠლ)")
+        
+    @commands.command()
+    async def info(self, ctx, member: discord.Member):
+        """Tells you some info about the member."""
+        fmt = " {0} joined on {0.joined_at} and has {1} roles."
+        await ctx.send(fmt.format(member, len(member.roles)))
+
+    @info.error
+    async def info_error(self, ctx, error):
+        """If User DNE, print error"""
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('I could not find that member...')
 
 
-# Sets up the cog
+
 def setup(bot):
+    """Sets up the cog"""
     bot.add_cog(AlexTest(bot))
