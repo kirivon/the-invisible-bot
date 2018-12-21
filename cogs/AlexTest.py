50133f1c0e6bafd37d72d941e0eec1fb32f856bf
alexisveryreal <44214923+alexisveryreal@users.noreply.github.com>
2018-12-20 20:44:59 -0800

    Just does a simple Ping command

---
+++ cogs/AlexTest.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+
+class AlexTest:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def ping(ctx):
+        #Returns ping when called
+        author = ctx.message.author.name
+        server = ctx.message.server.name
+        await bot.say("Ping from {} from {}!".format(author,server))
+
+
+
+
+# Sets up the cog
+def setup(bot):
+    bot.add_cog(Test(bot))
