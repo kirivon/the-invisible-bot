24b80eb0e515e18c90c0da0000f21c3f04d28544
jamesEmerson <vo.an.2503@gmail.com>
2018-12-22 00:29:45 -0800

    Add comments for test + Add Bam's file
    Practiced adding comments following the PEP8 guild in the test.py
    +
    adding the BamTest.py, which says hello when implmented

---
+++ cogs/BamTest.py
@@ -0,0 +1,42 @@
+"""
+Testing of how many times a client greets the bot
+"""
+
+
+# Author: Bamboozled
+
+import discord  					# import discord files?
+from discord.ext import commands	# import commands from it?
+# from redis import Redis, RedisError # maybe not
+
+# =============================================================================
+# Types and constants
+# =============================================================================
+
+# =============================================================================
+# Base decision tree
+# =============================================================================
+
+class BamboozledTest:
+	""" 0.1. class Lamb will now prompt a user greeting for now
+
+		Avoid using: N/A
+		Arguments: N/A
+	"""
+
+	@abstractmethod
+	def __init__(self,
+				 bot):
+		self.bot = bot
+
+	@commands.command()
+	async def hello(self, ctx):
+		""" 0.1. Saying "hello"
+		"""
+		greeting = "Hello, my dude"
+		await ctx.send(greeting)
+
+def setup(bot):
+	""" not sure what this does, setting up?
+	"""
+	bot.add_cog(BamboozledTest)
