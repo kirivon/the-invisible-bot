5a556914bdc20d341a2639005422dbbba2c45f40
brianbw <32603758+brianbw@users.noreply.github.com>
2018-12-22 01:02:13 -0800

    Update BrianTest.py

--- cogs/BrianTest.py
+++ cogs/BrianTest.py
@@ -1,12 +1,19 @@
 # Author: Brian Wong
 
 import discord
+import random
+from discord.ext import commands
 
 class BrianTest:
 
     def __init__(self, bot):
         self.bot = bot
         
+    @commands.command
+    async.def rngesus(self, ctx, range):
+        await ctx.send("Your number is " + random.randint(1, range))
+    
+    
     
     
   
