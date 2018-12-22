420ec7c4bf33a5a5748aab8eaf859c5d30d65eea
kirivon <kirivon@gmail.com>
2018-12-22 07:10:15 +0000

    update from ssh

--- cogs/AlexTest.py
+++ cogs/AlexTest.py
@@ -1,40 +1,90 @@
+from datetime import datetime
 import discord
 from discord.ext import commands
 
 
 class AlexTest:
-    """Class is just basic bot and user interaction"""
-    
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def pong(self, ctx):
-        """Bot command that returns the name of the invoker 
-        and the name of the server"""
-        author = ctx.author.name
-        server = ctx.guild.name
-        await ctx.send("Pong from {} from {}!".format(author, server))
+    async def latency(self, ctx):
+        """Bot command that returns the latency in ms"""
+        ping_ = self.bot.latency
+        ping = round(ping_ * 1000)
+        await ctx.send(f"Your ping is {ping}ms")
     
     @commands.command()
-    async def punch(self, ctx, user: discord.Member):
-        """Punches the user mepntions"""
-        await ctx.send("ONE PUNCH! And " + user.mention + "is out! ლ(ಠ益ಠლ)")
-        
+    async def punch(self,ctx, user: discord.Member):
+        """Punches the user mentioned"""
+        await ctx.send("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
+
+    
     @commands.command()
     async def info(self, ctx, member: discord.Member):
         """Tells you some info about the member."""
-        fmt = " {0} joined on {0.joined_at} and has {1} roles."
-        await ctx.send(fmt.format(member, len(member.roles)))
+        if member is ctx.message.author:
+            pronoun = "Your"
+        else:
+            pronoun = "Their"
+        name = f"{member.name}#{member.discriminator}"
+        status = member.status
+        joined = member.joined_at
+        role = member.top_role
+        msg = "{0} name is {1}. {0} status is {2}. They joined at {3}. {0} rank is {4}."
+        await ctx.send(msg.format(pronoun, name, status, joined, role))
+
+    @commands.command()
+    async def currtime(self, ctx):
+        """Returns current 12 hour local time"""
+        now = datetime.now()
+        msg = now.strftime("Current time: %I:%M:%S %p")
+        await ctx.send(msg)
+
+
+
+    @commands.command()
+    async def roles(self, ctx, member: discord.Member):
+        """Tells you a member's roles."""
+        msg = " {0}'s highest role is {0.top_role}."
+        await ctx.send(msg.format(member))
+
+    @commands.command()
+    async def mentionable(self, ctx, role: discord.Role):
+        """checks if a role is mentionable and then mentions them"""
+        if role.mentionable:
+            msg = " {0} is able to mentioned.... mentioning now {0.mention}."
+        else:
+            msg = " {0} can not be mentioned :c. "
+        await ctx.send(msg.format(role))
+
+    @commands.command()
+    async def benhammer(self, ctx, member: discord.Member):
+        """A test ban function, does not actually ban just sends a message"""
+        if member is ctx.message.author:
+            await ctx.channel.send("You cannot ban yourself!")
+            return
+        await ctx.send(f"{member} is beNNed  ̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿ !")
+
+    
+    @commands.command()
+    async def avatar(self, ctx, user: discord.User):
+        """Create URL of the persons avatar"""
+        msg = " URL being created for user ( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°) " + user.avatar_url
+        await ctx.send(msg)
+
+
 
     @info.error
     async def info_error(self, ctx, error):
-        """If User DNE, print error"""
+        """If user DNE, prints error"""
         if isinstance(error, commands.BadArgument):
-            await ctx.send('I could not find that member...')
+            await ctx.send('I could not find that member')
 
 
 
+
+# Sets up the cog
 def setup(bot):
-    """Sets up the cog"""
     bot.add_cog(AlexTest(bot))
