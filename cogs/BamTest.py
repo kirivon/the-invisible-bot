9d7179ce8a18c7a994c0cf3b6e4a10372350382b
jamesEmerson <vo.an.2503@gmail.com>
2018-12-23 01:55:01 -0800

    Update BamTest.py

--- cogs/BamTest.py
+++ cogs/BamTest.py
@@ -34,18 +34,19 @@ class BamboozledTest:
         0.1. class Lamb will now prompt a user greeting for now
     """
 
-    greeting = [    "(╯°□°)╯︵ ┻━┻ GTFO",
-                    "༼;´༎ຶ ۝ ༎ຶ༽ *gasp",
-                    "\m/...(>.<)…\m/ └[∵┌]└[ ∵ ]┘[┐∵]┘ OH YEAH, MY DUDE!!!!!", 
-                    "( ︶︿︶)_╭∩╮ ",
-                    "| (• ◡•)| (❍ᴥ❍ʋ ) darn right, mah homie!",
-                    "\"(Ó_Ò\")\"  oh....kay" 
-                ]  
+
 
     def __init__(self, 
                  bot):       # view: python3 is good, view: python is bad for sublime
         self.bot = bot
-
+        greeting = [    "(╯°□°)╯︵ ┻━┻ GTFO",
+                        "༼;´༎ຶ ۝ ༎ຶ༽ *gasp",
+                        "\m/...(>.<)…\m/ └[∵┌]└[ ∵ ]┘[┐∵]┘ OH YEAH, MY DUDE!!!!!", 
+                        "( ︶︿︶)_╭∩╮ ",
+                        "| (• ◡•)| (❍ᴥ❍ʋ ) darn right, mah homie!",
+                        "\"(Ó_Ò\")\"  oh....kay" 
+                    ]  
+                    
     async def on_message(self, ctx, greeting): 
         """Defines bot behavior when a message is posted to channel"""
         # Do something
