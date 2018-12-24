9eb6ad7988813f298945596e0921cc58bc04ad5a
jamesEmerson <vo.an.2503@gmail.com>
2018-12-24 15:28:17 -0800

    Update BamTest.py

--- cogs/BamTest.py
+++ cogs/BamTest.py
@@ -115,6 +115,18 @@ class BamboozledTest:
         mood = random.randint(1, 6)
         await ctx.send(self.uwu_greeting[mood - 1])  # array max index is n -1
 
+    @commands.command()
+    async def sure(self, ctx, arg)
+        """ 0.1. Spongebod Bob mocking tone
+
+            Argument: message
+        """
+
+        mocking = arg
+        mocking = mocking.lower()
+        for index in mocking:
+            mocking[index] = mocking[index].upper()
+        await ctx.send(mocking)
 
 # Sets up the cog
 def setup(bot):
