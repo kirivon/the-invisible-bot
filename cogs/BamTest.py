1a5052deca436d566efad47beab3eb7a51e0d71b
jamesEmerson <vo.an.2503@gmail.com>
2018-12-25 01:27:06 -0800

    Update BamTest.py

--- cogs/BamTest.py
+++ cogs/BamTest.py
@@ -139,8 +139,8 @@ class BamboozledTest:
         mocking_first_letter = mocking[0]
         mocking = mocking[1:]
         mocking = ''.join(choice((str.upper, str.lower))(index) for index in mocking)
-        Spongebod = "<:mockingspongebobbb:526939558915604490>"
-        await ctx.send("*{}{}*   {}".format(mocking_first_letter, mocking, Spongebod))
+        # Spongebob = "<:mockingspongebobbb:526939558915604490>"
+        await ctx.send("*{}{}*   {}".format(mocking_first_letter, mocking))
         
 
 # Sets up the cog
