78325ff3b3fd637730e9241898d8fb226ada2c16
jamesEmerson <vo.an.2503@gmail.com>
2018-12-25 16:22:41 -0800

    Update BamTest.py

--- cogs/BamTest.py
+++ cogs/BamTest.py
@@ -141,7 +141,7 @@ class BamboozledTest:
         mocking = mocking[1:]
         mocking = ''.join(choice((str.upper, str.lower))(index) for index in mocking)
         spongebob_meme = "https://cdn.discordapp.com/attachments/505902755097870346/527064513120436235/image0.png"
-        await ctx.send("{} \n".format(spongebob_meme)
+        await ctx.send("{} \n".format(spongebob_meme))
         await ctx.send("*{}{}*".format(mocking_first_letter, mocking))
         
 
