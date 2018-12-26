7b2e4625630baa3c424fb2c7892aa74a9725578e
alexisveryreal <alexmaple20@gmail.com>
2018-12-26 11:29:50 -0800

    added a print when setting the bot status

--- cogs/AlexTest.py
+++ cogs/AlexTest.py
@@ -127,6 +127,7 @@ class AlexTest:
         if message:
             game = discord.Game(message)
             await self.bot.change_presence(status=discord.Status.online, activity=game)
+            await ctx.send("set bot status to..." + game)
         else:
             game = discord.Game("with unicode faces")
             await self.bot.change_presence(status=discord.Status.online, activity=game)
