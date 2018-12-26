c448e5437b8064bc0c90fa7c8ab98f2704ede995
alexisveryreal <alexmaple20@gmail.com>
2018-12-26 11:53:16 -0800

    added a delete command to botstatus func

--- cogs/AlexTest.py
+++ cogs/AlexTest.py
@@ -128,6 +128,7 @@ class AlexTest:
             game = discord.Game(message)
             await self.bot.change_presence(status=discord.Status.online, activity=game)
             await ctx.send("set bot status to..." + message)
+            await ctx.message.delete()
         else:
             game = discord.Game("with unicode faces")
             await self.bot.change_presence(status=discord.Status.online, activity=game)
