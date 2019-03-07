902c14b0b5c357ac8aac064f385aebd76fde31f5
alexisveryreal <alexmaple20@gmail.com>
2019-03-07 15:22:53 -0800

    commented out the default bot status

--- cogs/AlexTest.py
+++ cogs/AlexTest.py
@@ -201,10 +201,10 @@ class AlexTest:
             await self.bot.change_presence(status=discord.Status.online,
                                            activity=game)
             await ctx.send("set bot status to..." + message)
-        else:
-            game = discord.Game("with unicode faces")
-            await self.bot.change_presence(status=discord.Status.online,
-                                           activity=game)
+        # else:
+        #     game = discord.Game("with unicode faces")
+        #     await self.bot.change_presence(status=discord.Status.online,
+        #                                    activity=game)
 
     # @commands.command()
     # async def flips(self, ctx):
