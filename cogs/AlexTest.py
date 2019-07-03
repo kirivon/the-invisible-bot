eec111236fed0071699ada7c35df8556bdd3cd09
alexisveryreal <alexmaple20@gmail.com>
2019-07-03 15:03:21 -0700

    Added angry birds quotes

--- cogs/AlexTest.py
+++ cogs/AlexTest.py
@@ -191,7 +191,8 @@ class AlexTest:
             'So, let\'s store the meaning of life, _42_, into the register',
             'And of course your compiler will give you a very useful message that will surely help you debug your program!',
             'I\'m sure all of you have been working on the assignment and not waiting for the night before..._Yeah right, ha ha_',
-            '12 dollar Frys special, go buy some real hardware'
+            '12 dollar Frys special, go buy some real hardware',
+            'The most important program written in human history, which is Angry Birds'
         ]
         if message == "LIST":
             em = discord.Embed(color=0xea7938)
