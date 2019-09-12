c13c708d23d0f52ce0719889b2e78d3e650f1265
alexisveryreal <alexmaple20@gmail.com>
2019-09-11 21:29:00 -0700

    Got rid of "with" in bot status message

--- cogs/AlexTest.py
+++ cogs/AlexTest.py
@@ -225,7 +225,7 @@ class AlexTest:
             game = discord.Game(message)
             await self.bot.change_presence(status=discord.Status.online,
                                            activity=game)
-            await ctx.send("set bot status to ' Playing with " + message + " ' ")
+            await ctx.send("set bot status to ' Playing " + message + " ' ")
         # else:
         #     game = discord.Game("with unicode faces")
         #     await self.bot.change_presence(status=discord.Status.online,
